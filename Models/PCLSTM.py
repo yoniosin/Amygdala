a9d43efd2b1fbcb0e6e61919796d3084ee03d46d
Yoni Osin <yoni.osin@gmail.com>
2019-07-28 12:48:40 +0300

    lstm working receives history

--- Models/PCLSTM.py
+++ Models/PCLSTM.py
@@ -2,19 +2,22 @@ import torch
 from torch import nn
 from torch.autograd import Variable
 
+
 class PCLSTMCell(nn.Module):
-    def __init__(self,  input_channels,  hidden_channels):
+    def __init__(self, input_channels, hidden_channels):
         super().__init__()
         self.input_channels = input_channels
         self.hidden_channels = hidden_channels
-        create_gate = lambda: nn.Linear(self.input_channels * 2 + self.hidden_channels, self.hidden_channels)
-        create_gates_dict = lambda :nn.ModuleDict({'forget_gate': create_gate(),
-                                                'input_gate': create_gate(),
-                                                'update_gate': create_gate(),
-                                                'output_gate': create_gate()})
+
+        def create_gate(): return nn.Linear(self.input_channels * 2 + self.hidden_channels, self.hidden_channels)
+
+        def create_gates_dict(): return nn.ModuleDict({'forget_gate': create_gate(),
+                                                       'input_gate': create_gate(),
+                                                       'update_gate': create_gate(),
+                                                       'output_gate': create_gate()})
+
         self.gates = {'regular': create_gates_dict(), 'transition': create_gates_dict()}
         self.initial_hidden = None
-
 
     def forward(self, x, h_prev, c_prev, transition=False):
         if self.initial_hidden is None:
@@ -23,24 +26,26 @@ class PCLSTMCell(nn.Module):
         stacked_inputs = torch.cat((x, h_prev), dim=-1)
         gates_dict = self.gates['transition'] if transition else self.gates['regular']
         f_t = nn.Sigmoid()(gates_dict['forget_gate'](stacked_inputs))
-        i_t = nn.Sigmoid()(gates_dict['input_gate'](stacked_inputs))
-        c_opt = nn.Tanh()(gates_dict['update_gate'](stacked_inputs))
-        o_t = nn.Sigmoid()(gates_dict['output_gate'](stacked_inputs))
+        i_t = nn.Sigmoid()(self.gates['regular']['input_gate'](stacked_inputs))
+        c_opt = nn.Tanh()(self.gates['regular']['update_gate'](stacked_inputs))
+        o_t = nn.Sigmoid()(self.gates['regular']['output_gate'](stacked_inputs))
 
         c_t = c_prev * f_t + i_t * c_opt
         h_t = o_t * nn.Tanh()(c_t)
 
         return h_t, c_t
 
+
 class PCLSTM(nn.Module):
-    def __init__(self, input_channels, hidden_channels,sub_sequence_len, num_layers=1):
+    def __init__(self, input_channels, hidden_channels, sub_sequence_len, num_layers=1):
         super().__init__()
         self.input_channels = input_channels
         self.hidden_channels = hidden_channels
         self.subsequence_len = sub_sequence_len
 
         self.cells = nn.ModuleList([PCLSTMCell(hidden_channels=self.hidden_channels[i],
-                                    input_channels=self.input_channels if i==0 else self.hidden_channels[i-1])
+                                               input_channels=self.input_channels if i == 0 else self.hidden_channels[
+                                                   i - 1])
                                     for i in range(num_layers)])
 
     def forward(self, x, y, h_0=None):
@@ -55,7 +60,7 @@ class PCLSTM(nn.Module):
                 x_i = x[..., t]
                 y_prev = y[..., t - 1] if t > 0 else torch.zeros(x_i.shape)
                 h = torch.cat((h, y_prev), dim=-1)
-                use_transition_gate = (t % self.subsequence_len) == self.subsequence_len - 1
+                use_transition_gate = t % self.subsequence_len == 0
                 h, c = cell(x_i, h, c, use_transition_gate)
                 inner_cell_out.append(h)
 
@@ -63,7 +68,6 @@ class PCLSTM(nn.Module):
 
         all_cells_output = torch.stack(cells_output, dim=1)
         return cells_output[-1], c
-
 
     def init_hidden(self, h_0, input_shape):
         if h_0:
@@ -73,6 +77,3 @@ class PCLSTM(nn.Module):
             zero_state = lambda j: Variable(torch.zeros(*input_shape, self.hidden_channels[j]))
             for i, cell in enumerate(self.cells):
                 cell.initial_hidden = [zero_state(i) for _ in range(2)]
-
-
-
